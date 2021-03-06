bb9c8f9a4499101f75a490f3f08539c28763f124
farrel-a <70206944+farrel-a@users.noreply.github.com>
2021-03-06 20:49:21 +0700

    Update Programming_Modul2_16520373.cpp
    Penambahan komentar dan penjelasan program

--- Programming_Modul2_16520373.cpp
+++ Programming_Modul2_16520373.cpp
@@ -2,13 +2,19 @@
 // PROGRAM Kalkulator Sederhana dan Kalkulator Integral
 // MODUL 2 PROGRAMMING SEKURO
 // SPESIFIKASI :
-/*Program kalkulator yang dapat menghitung operasi sederhana dan
-integral dengan pendekatan riemann dengan 100 partisi*/
+/*Program kalkulator yang dapat menghitung operasi sederhana dari dua
+bilangan integer atau double floating point dan menghitung
+integral tentu menggunakan pendekatan riemann dengan 100 partisi*/
 #include <iostream>
 #include <cmath>
 
 using namespace std;
 
+// fungsi kalkulator sederhana
+// parameter input:
+// a : angka pertama
+// op : operator
+// b : angka kedua
 double bcalc (double a, char op, double b)
 {double calcresult;
     if (op == '+'){
@@ -31,11 +37,18 @@ double bcalc (double a, char op, double b)
         }
     return calcresult;
 }
-
+// fungsi integral
+// parameter input (berurut) :
+// l : batas bawah (lower bound)
+// u : batas atas (upper bound)
+// n1 : derajat polinom pembilang
+// arr1[] : array penyimpan koefisien variabel x pada pembilang (numerator)
+// n2 : derajat polinom penyebut
+// arr2 [] : array penyimpan koefisien variabel x pada penyebut (denominator)
 double integral(double l, double u, int n1, double arr1[], int n2, double arr2[])
 {
     // integral riemann trapesium dengan 1000 partisi
-    double f_lowern, f_uppern, f_lowerd, f_upperd, delta, i, result, f_result, res_upper, res_lower;
+    double f_lowern, f_uppern, f_lowerd, f_upperd, delta, i, result, res_upper, res_lower;
     int j,k;
     delta = (u-l)/1000;
     result = 0;
@@ -70,20 +83,20 @@ double integral(double l, double u, int n1, double arr1[], int n2, double arr2[]
 }
 int main()
 {
-    int N1, N2, i;
-    char com,op;
-    double n1,n2, iresult, lb, ub, cresult;
-    bool flag;
+    int N1, N2, i;  // N1 input(N1), N2 input(N2), i bil.int for loop
+    char com,op; // com input command, op input operator
+    double n1,n2, iresult, lb, ub; //n1 & n2 : num1 & num2 kalkulator sederhana, iresult : integral result, lb : lower bound, ub : upper bound (integral)
+    bool flag; // flag while loop
     flag = true;
     while(flag==true)
     {
     cout << "Welcome to Calculator++\nInsert\n'c' for basic caclulator\n'i' for integral calculator\n'e' for exit\nInsert Command: " ;
-    cin >> com;
-    if (com == 'e')
+    cin >> com;  // input command
+    if (com == 'e' || com == 'E')   //kondisional penyelesaian program/exit program
         {
             flag = false;
         }
-    else if (com == 'c')
+    else if (com == 'c' || com == 'C') //kondisional kalkulator sederhana
     {
         cout << "\nCommand List for Basic Calculator: " << endl;
         cout << "'+' for addition\n'-' for substraction\n'*' for multiplication\n'/' for division\n'p' for power" << endl;
@@ -93,24 +106,24 @@ int main()
         cin >> op;
         cout << "Insert second number: ";
         cin >> n2;
-        cout << "\nResult = " << bcalc(n1, op, n2) << "\n" << endl;
+        cout << "\nResult = " << bcalc(n1, op, n2) << "\n" << endl;  // output kalkulator sederhana
     }
-    else if (com == 'i' || com == 'I')
+    else if (com == 'i' || com == 'I') //kondisional kalkulator integral
     {
         cout << "\nYou are about to define a function" << endl;
         cout << "Please define a numerator" <<endl;
-        cout << "Insert numerator degree: ";   //pembagi derajat berapa?
+        cout << "Insert numerator degree: ";   // pembilang derajat berapa?
         cin >> N1;
-        double arrnum1[N1+1];      //tempat koefisien dalam array
+        double arrnum1[N1+1];      // tempat penyimpanan koefisien pembilang dalam array
         for (i = N1; i>=0; i--)
         {
             cout << "Insert coefficient of x^" << i << ": ";
             cin >> arrnum1[N1-i];
         }
         cout << "\nPlease define a denominator" << endl;
-        cout << "Insert denominator degree: ";
+        cout << "Insert denominator degree: "; // penyebut derajat berapa?
         cin >> N2;
-        double arrnum2[N2+1];
+        double arrnum2[N2+1];  // tempat penyimpanan koefisien penyebut dalam array
         for (i = N2; i>=0; i--)
         {
             cout << "Insert coefficient of x^" << i << ": ";
@@ -121,9 +134,10 @@ int main()
         cout << "Insert upper bound: "; //batas atas
         cin >> ub;
         iresult = integral(lb, ub, N1, arrnum1, N2, arrnum2);
-        cout << "\nResult = " << iresult <<"\n"<<endl;
+        cout << "\nResult = " << iresult <<"\n"<<endl; // output integral tentu
     }
-    else {
+    else // kondisional input command tidak sesuai
+    {
         cout << endl;
         cout << "Invalid Command" << endl;
         cout << endl;
